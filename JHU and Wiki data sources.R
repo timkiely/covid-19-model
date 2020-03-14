b009e9ac0302ebc6734e59fea4a98b4191f139c8
Tim Kiely <timothy.j.kiely@gmail.com>
2020-03-14 13:07:59 -0400

    added JHU sources

--- JHU and Wiki data sources.R
+++ JHU and Wiki data sources.R
@@ -19,8 +19,10 @@ us_confirmed_long_jhu <- read_csv(jhu_url) %>% rename(province = "Province/State
                   country_region, Lat, Long), names_to = "Date", values_to = "cumulative_cases") %>% 
   # adjust JHU dates back one day to reflect US time, more or
   # less
-  mutate(Date = mdy(Date) - days(1)) %>% filter(country_region == 
-                                                  "US") %>% arrange(province, Date) %>% group_by(province) %>% 
+  mutate(Date = mdy(Date) - days(1)) %>% 
+  filter(country_region == "US") %>% 
+  arrange(province, Date) %>% 
+  group_by(province) %>% 
   mutate(incident_cases = c(0, diff(cumulative_cases))) %>% 
   ungroup() %>% select(-c(country_region, Lat, Long, cumulative_cases)) %>% 
   filter(str_detect(province, "Diamond Princess", negate = TRUE))
